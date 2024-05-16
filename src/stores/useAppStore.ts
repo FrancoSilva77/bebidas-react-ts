56b803bb83a6e6a9954ffa6aef0a41c1cf1c8d57
Franco Silva <franco_tigres18@hotmail.com>
2024-05-15 21:01:48 -0600

    feat(Store favorites):

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,9 +1,11 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 import { RecipesSliceType, createRecipesSlice } from './recipeSlice';
+import { FavoritesSliceType, createFavoritesSlice } from './favoritesSlice';
 
-export const useAppStore = create<RecipesSliceType>()(
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(
   devtools((...a) => ({
     ...createRecipesSlice(...a),
+    ...createFavoritesSlice(...a),
   }))
 );
