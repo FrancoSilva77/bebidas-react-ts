e1b8f31c1e01379ff7ac0e3da23409ae1286c7b8
Franco Silva <franco_tigres18@hotmail.com>
2024-05-15 21:14:34 -0600

    feat(Store favorites):Agregar a Favoritos

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,6 +7,7 @@ export default function Modal() {
   const modal = useAppStore((state) => state.modal);
   const closeModal = useAppStore((state) => state.closeModal);
   const selectedRecipe = useAppStore((state) => state.selectedRecipe);
+  const handleClickFavorite = useAppStore((state) => state.handleClickFavorite);
 
   const renderIngredients = () => {
     const ingredients: JSX.Element[] = [];
@@ -97,7 +98,7 @@ export default function Modal() {
                       </button>
                       <button
                         type="button"
-                        // onClick={}
+                        onClick={() => handleClickFavorite(selectedRecipe)}
                         className=" w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500"
                       >
                         Agregar a Favoritos
